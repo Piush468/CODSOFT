45fd39599dc0d073a5111081e958b13c7badaa71
Piush468 <piushg110@gmail.com>
2025-10-08 20:23:58 +0530

    Add files via upload

---
+++ Todo List/todoproject/todoapp/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Task
+from .forms import TaskForm
+
+def task_list(request):
+    tasks = Task.objects.all()
+    form = TaskForm()
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    return render(request, 'task_list.html', {'tasks': tasks, 'form': form})
+
+def task_update(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    form = TaskForm(instance=task)
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    return render(request, 'task_update.html', {'form': form})
+
+def task_delete(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == 'POST':
+        task.delete()
+        return redirect('task_list')
+    return render(request, 'task_delete.html', {'task': task})
